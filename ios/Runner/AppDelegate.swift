f167e8c37be5ca1f91dcc5215485267a663da543
girishmobile <macmini01062019@gmail.com>
2025-09-18 14:30:05 +0530

    some changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,64 @@
 import Flutter
 import UIKit
 import FirebaseCore
-import flutter_local_notifications
+import FirebaseMessaging
+
+
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+      
+      // Configure Firebase
+         FirebaseApp.configure()
+     
+      Messaging.messaging().delegate = self
+      
+      
 
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self
-    }
+//    if #available(iOS 10.0, *) {
+//      UNUserNotificationCenter.current().delegate = self
+//    }
 
+      // Register for APNs
+          if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+              options: authOptions,
+              completionHandler: { _, _ in })
+          } else {
+            let settings: UIUserNotificationSettings =
+              UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+          }
+          
+          application.registerForRemoteNotifications()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    // MARK: - FCM Delegate
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("✅ Firebase registration token: \(String(describing: fcmToken))")
+        // TODO: send token to server if needed
+    }
+
+    // MARK: - APNs Token
+    override func application(_ application: UIApplication,
+                              didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+
+    // MARK: - Foreground Notification Handling
+    @available(iOS 10, *)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 }
