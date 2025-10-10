637c3e6cda51eb44bec2c3f0b864cf8483314312
girishmobile <macmini01062019@gmail.com>
2025-10-10 18:21:38 +0530

    minor changes

--- lib/feature/admin/admin_login_page.dart
+++ lib/feature/admin/admin_login_page.dart
@@ -25,141 +25,129 @@ class AdminLoginPage extends StatelessWidget {
     var isMobile = Responsive.isMobile(context);
     final formLoginKey = GlobalKey<FormState>();
     return commonScaffold(
-
-      backgroundColor: colorBg,
-      body: Consumer2<ThemeProvider, LoginProvider>(
-        builder: (context, themeProvider, provider, child) {
+      backgroundColor: Colors.white,
+      body: Consumer<LoginProvider>(
+        builder: (context, provider, child) {
           return Stack(
             children: [
               Center(
                 child: SingleChildScrollView(
                   physics: const BouncingScrollPhysics(),
                   padding: const EdgeInsets.all(20),
-                  child: Consumer<LoginProvider>(
-                    builder: (context, provider, child) {
-                      return commonPopScope(
-                        onBack: () {
-                          provider.resetState();
-                        },
-                        child: LayoutBuilder(
-                          builder: (context, constraints) {
-                            double width = constraints.maxWidth;
-                            double containerWidth;
-                            double padding;
-                            if (width >= 1200) {
-                              // Desktop
-                              containerWidth = width * 0.25;
-                              padding = 36;
-                            } else if (width >= 800) {
-                              // Tablet
-                              containerWidth = width * 0.5;
-                              padding = 30;
-                            } else {
-                              // Mobile
-                              containerWidth = width * 0.9;
-                              padding = 24;
-                            }
-                            return Form(
-                              key: formLoginKey,
-                              child: Container(
-                                padding: EdgeInsets.all(padding),
-                                width: containerWidth,
-                                decoration: commonBoxDecoration(
-                                  borderRadius: 12,
-                                  borderColor: themeProvider.isDark
-                                      ? Colors.white54
-                                      : colorBorder,
-                                  color: themeProvider.isDark
-                                      ? colorBorder
-                                      : Colors.white,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black12,
-                                      blurRadius: 8,
-                                      offset: Offset(0, 2),
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    SizedBox(height: 20),
-                                    Align(
-                                      alignment: AlignmentGeometry.center,
-                                      child: commonAssetImage(
-                                        icAppLogo,
-                                        height: isMobile?62:72,
-                                        width: size.width * 0.7,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 30),
-                                    commonHeadingText(
-                                      text: "Welcome back 👋 ",
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.w700,
-                                      color: themeProvider.isDark
-                                          ? Colors.white
-                                          : colorLogo,
-                                    ),
-                                    const SizedBox(height: 2),
-                                    commonDescriptionText(
-                                      text: "E-Commerce Admin Panel",
-                                    ),
-                                    const SizedBox(height: 24),
-                                    commonTextField(
-                                      hintText: "Email",
-                                      controller: provider.tetEmail,
-                                      validator: validateEmail,
-                                      prefixIcon: commonPrefixIcon(
-                                        image: icEmail,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 24),
-                                    commonTextField(
-                                      hintText: "Password",
-                                      controller: provider.tetPassword,
-                                      validator: validatePassword,
-                                      obscureText: provider.obscurePassword,
-                                      maxLines: 1,
-                                      prefixIcon: commonPrefixIcon(
-                                        image: icPassword,
-                                      ),
-                                      suffixIcon: GestureDetector(
-                                        onTap: () {
-                                          // provider.toggleCurrentPassword();
-                                        },
-                                        child: Icon(
-                                          provider.obscurePassword
-                                              ? Icons.visibility_off
-                                              : Icons.visibility,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(height: 24),
-                                    commonButton(
-                                      text: "Login",
-                                      width: size.width,
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                AdminHomePage(),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      );
+                  child: commonPopScope(
+                    onBack: () {
+                      provider.resetState();
                     },
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        double width = constraints.maxWidth;
+                        double containerWidth;
+                        double padding;
+                        if (width >= 1200) {
+                          // Desktop
+                          containerWidth = width * 0.25;
+                          padding = 36;
+                        } else if (width >= 800) {
+                          // Tablet
+                          containerWidth = width * 0.5;
+                          padding = 30;
+                        } else {
+                          // Mobile
+                          containerWidth = width * 0.9;
+                          padding = 24;
+                        }
+                        return Form(
+                          key: formLoginKey,
+                          child: Container(
+                            padding: EdgeInsets.all(padding),
+                            width: containerWidth,
+                            decoration: commonBoxDecoration(
+                              borderRadius: 12,
+                              borderColor: colorBorder,
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 8,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 20),
+                                Align(
+                                  alignment: AlignmentGeometry.center,
+                                  child: commonAssetImage(
+                                    icAppLogo,
+                                    height: isMobile ? 62 : 72,
+                                    width: size.width * 0.7,
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                                commonHeadingText(
+                                  text: "Welcome back 👋 ",
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                  color: colorLogo,
+                                ),
+                                const SizedBox(height: 2),
+                                commonDescriptionText(
+                                  text: "E-Commerce Admin Panel",
+                                ),
+                                const SizedBox(height: 24),
+                                commonTextField(
+                                  hintText: "Email",
+                                  controller: provider.tetEmail,
+                                  validator: validateEmail,
+                                  prefixIcon: commonPrefixIcon(image: icEmail),
+                                ),
+                                const SizedBox(height: 24),
+                                commonTextField(
+                                  hintText: "Password",
+                                  controller: provider.tetPassword,
+                                  validator: validatePassword,
+                                  obscureText: provider.obscurePassword,
+                                  maxLines: 1,
+                                  prefixIcon: commonPrefixIcon(
+                                    image: icPassword,
+                                  ),
+                                  suffixIcon: GestureDetector(
+                                    onTap: () {
+                                      print("Toggle");
+                                      provider.togglePassword();
+                                    },
+                                    child: Icon(
+                                      provider.obscurePassword
+                                          ? Icons.visibility_off
+                                          : Icons.visibility,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 24),
+                                commonButton(
+                                  text: "Login",
+                                  width: size.width,
+                                  onPressed: () {
+                                    if (formLoginKey.currentState?.validate() ==
+                                        true) {
+                                      Navigator.pushNamedAndRemoveUntil(
+                                        context,
+                                        RouteName.adminHomePage,
+                                        (Route<dynamic> route) => false,
+                                      );
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
               ),
